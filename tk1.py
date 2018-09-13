54790b3081b8065bdf3e314afff28f81f518fdb9
tndr1 <a01654827@itesm.mx>
2018-09-12 22:46:50 -0500

    asdf.,ño

--- tk1.py
+++ tk1.py
@@ -1,6 +1,12 @@
+
+#https://www.tutorialspoint.com/python/python_gui_programming.htm
+
 from tkinter import *
 from tkinter import messagebox
 
+
+User= "Tandoori7"
+Passwd="AAA"
 
 def printingTest():
     print("Testing 1 2 3")
@@ -10,18 +16,26 @@ window = Tk()
 
 window.title("MANAGMENT")
 
-window.geometry('350x200')
+window.geometry('700x400')
 
 
 
-BTN1 = Button(window, text =("TEST")  , command=printingTest)
+BTN1 = Button(window, text =("TESTING")  , command=printingTest)
 #BTN1.pack()
 BTN2 = Button(window, text =("TEST")  , command=printingTest)
 #BTN2.pack()
 BTN3 = Button(window, text =("TEST")  , command=printingTest)
 #BTN3.pack()
 
+Labl1 = Label(window , text="Hola a todos")
+
+
+Ent1 = Entry(window, text="Inserta Usuario")
 BTN1.grid(column=2, row=0)
 BTN2.grid(column=3, row=0)
 BTN3.grid(column=4, row=0)
+
+Ent1.grid(column=5, row=0)
 window.mainloop()
+
+
