7fe2f35769518f8acca2c575af272eddb24e7c5f
tndr <a01654827@itesm.mx>
2018-09-14 09:18:27 -0500

    dasdf

--- InicioDeSesion.py
+++ InicioDeSesion.py
@@ -5,11 +5,11 @@ from tkinter import messagebox
 
 
 ValidUsser= "Tandoori7"
-Passwd="AAA"
+ValidPasswd="AAA"
 
 def printingTest():
     print("Testing 1 2 3")
-    messagebox.showinfo('')
+    messagebox.showinfo('OwO')
 
 def gettingUsser():
     Usser= Ent1.get()
@@ -18,8 +18,9 @@ def gettingUsser():
 def gettingPassword():
     Password = Ent2.get()
 
-def CloseWindow(self):
-    self.destroy()
+def CloseWindow():
+    global window
+    window.quit()
 
 window = Tk()
 
@@ -28,18 +29,17 @@ window.title("MANAGMENT")
 window.geometry('400x200')
 
 
-
-BTN1 = Button(window, text =("INICIAR SESIÓN")  , command=printingTest)
-BTN2 = Button(window, text=("Crear Cuenta Nueva"), command=gettingPassword)
-BTN3 = Button(window, text="Cerrar", commad=CloseWindow(window))
-
-
 Labl1 = Label(window , text="Introduce tu Usuario")
 Labl2 = Label(window, text="Introduce Contraseña")
 
 
 Ent1 = Entry(window)
 Ent2 = Entry(window)
+
+BTN1 = Button(window, text =("INICIAR SESIÓN")  , command=gettingUsser())
+BTN2 = Button(window, text=("Crear Cuenta Nueva"), command=gettingPassword)
+BTN3 = Button(window, text="Cerrar", commad=CloseWindow())
+
 
 BTN1.grid(column=1, row=3)
 BTN2.grid(column=0, row=3)
